8ccb6a81d217090739ba16db1314913b66736bc4
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-10-17 11:31:38 +0300

    switched to openweather

--- assets/api/weather.js
+++ assets/api/weather.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const apiKey = 'beaa71f3be8743dc967131426251510'
-const forecastEndpoint = params => `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no`
-const locationsEndpoint = params => `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+const apiKey = '1cf838aa8644549473bdf55ad4147ca1'
+const weatherUrlBase = 'https://api.openweathermap.org/data/2.5/'
+
+const weatherEndpoint = params => `${weatherUrlBase}weather?q=${params.query}&units=metric&appid=${apiKey}`
 
 const apiCall = async (endpoint)=>{
     const options = {
@@ -10,8 +11,8 @@ const apiCall = async (endpoint)=>{
         url: endpoint
     }
     try{
-const response = await axios.request(options)
-return response.data
+      const response = await axios.request(options)
+      return response.data
     }catch(err){
         console.log('err',err)
         return null;
@@ -19,9 +20,9 @@ return response.data
 }
 
 export const fetchForecast = params =>{
-    return apiCall(forecastEndpoint(params))
+    return apiCall(weatherEndpoint(params))
 }
 
 export const fetchLocations = params =>{
-    return apiCall(locationsEndpoint(params))
+    return apiCall(weatherEndpoint(params))
 }
