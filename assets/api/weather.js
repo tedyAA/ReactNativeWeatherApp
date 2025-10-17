9921fc9c2cf08e5d24b4943a9b8a4b5242fde5c8
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-10-17 11:58:40 +0300

    added forecast

--- assets/api/weather.js
+++ assets/api/weather.js
@@ -1,28 +1,31 @@
-import axios from "axios";
+import axios from 'axios';
 
-const apiKey = '1cf838aa8644549473bdf55ad4147ca1'
-const weatherUrlBase = 'https://api.openweathermap.org/data/2.5/'
+const apiKey = '1cf838aa8644549473bdf55ad4147ca1';
+const weatherUrlBase = 'https://api.openweathermap.org/data/2.5/';
 
-const weatherEndpoint = params => `${weatherUrlBase}weather?q=${params.query}&units=metric&appid=${apiKey}`
+const weatherEndpoint = (params) =>
+  `${weatherUrlBase}weather?q=${params.query}&units=metric&appid=${apiKey}`;
+const forecastEndpoint = (params) =>
+  `${weatherUrlBase}forecast?q=${params.query}&units=metric&appid=${apiKey}`;
 
-const apiCall = async (endpoint)=>{
-    const options = {
-        method: 'GET',
-        url: endpoint
-    }
-    try{
-      const response = await axios.request(options)
-      return response.data
-    }catch(err){
-        console.log('err',err)
-        return null;
-    }
-}
+const apiCall = async (endpoint) => {
+  const options = {
+    method: 'GET',
+    url: endpoint,
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data;
+  } catch (err) {
+    console.log('err', err);
+    return null;
+  }
+};
 
-export const fetchForecast = params =>{
-    return apiCall(weatherEndpoint(params))
-}
+export const fetchForecast = (params) => {
+  return apiCall(forecastEndpoint(params));
+};
 
-export const fetchLocations = params =>{
-    return apiCall(weatherEndpoint(params))
-}
+export const fetchLocations = (params) => {
+  return apiCall(weatherEndpoint(params));
+};
