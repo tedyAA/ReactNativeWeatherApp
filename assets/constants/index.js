871ff4bbfd59f2841c9752ac94ba2180eeb7e670
Teodora Angelova <43522056+tedyAA@users.noreply.github.com>
2025-10-17 11:58:55 +0300

    added new helper and prettier

--- assets/constants/index.js
+++ assets/constants/index.js
@@ -1,27 +1,33 @@
 export const weatherImages = {
-  'Thunderstorm': require('../images/heavyrain.png'),
-  'Drizzle': require('../images/moderaterain.png'),
-  'Rain': require('../images/heavyrain.png'),
-  'Snow': require('../images/snow.png'),
-  'Mist': require('../images/moderaterain.png'),
-  'Smoke': require('../images/cloud.png'),
-  'Haze': require('../images/cloud.png'),
-  'Dust': require('../images/cloud.png'),
-  'Fog': require('../images/cloud.png'),
-  'Sand': require('../images/mist.png'),
-  'Ash': require('../images/mist.png'),
-  'Squall': require('../images/mist.png'),
-  'Tornado': require('../images/tornado.png'),
-  'Clear': require('../images/sun.png'),
-  'Clouds': require('../images/cloud.png'),
-  'other': require('../images/moderaterain.png'),
-} 
+  Thunderstorm: require('../images/heavyrain.png'),
+  Drizzle: require('../images/moderaterain.png'),
+  Rain: require('../images/heavyrain.png'),
+  Snow: require('../images/snow.png'),
+  Mist: require('../images/moderaterain.png'),
+  Smoke: require('../images/cloud.png'),
+  Haze: require('../images/cloud.png'),
+  Dust: require('../images/cloud.png'),
+  Fog: require('../images/cloud.png'),
+  Sand: require('../images/mist.png'),
+  Ash: require('../images/mist.png'),
+  Squall: require('../images/mist.png'),
+  Tornado: require('../images/tornado.png'),
+  Clear: require('../images/sun.png'),
+  Clouds: require('../images/cloud.png'),
+  other: require('../images/moderaterain.png'),
+};
 
 export function convertUnixTime(unixTime) {
- 
   const date = new Date(unixTime * 1000);
   const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
   return `${hours}:${minutes}`;
+}
+
+export function getWeekdayAbbrev(unixTime) {
+  const date = new Date(unixTime * 1000);
+  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  return weekdays[date.getDay()];
 }
